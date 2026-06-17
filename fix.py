e9acafa200c989ed85ba9427aa2037dd13005fdc
morounfoluwa-jpg <mfoluchristine11@gmail.com>
2026-06-17 02:24:04 -0500

    Clean up repo, add README

--- fix.py
+++
@@ -1,6 +0,0 @@
-with open('src/dashboard.py', 'rb') as f:
-    content = f.read()
-content = content.decode('utf-8', errors='ignore')
-with open('src/dashboard.py', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Fixed!')
